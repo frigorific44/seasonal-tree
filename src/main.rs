ed18d3dcf526f57825cc5e2746fd09787d716651
Derek Enlow <derekenlow@gmail.com>
2026-07-11 14:47:33 -0600

    Project initialized.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+struct Model {
+    _window: Entity,
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build();
+    Model { _window }
+}
+
+fn update(_app: &App, _model: &mut Model) {}
+
+fn view(app: &App, _model: &Model) {
+    let draw = app.draw();
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEEL_BLUE);
+}
